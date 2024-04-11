1ad228845146f46a110becbad87dacc5bcd37d9b
YounHooooo <yh6316l@naver.com>
2024-04-11 17:33:58 +0900

    Movie

--- src/App.js
+++ src/App.js
@@ -1,35 +1,58 @@
-// 클래스형 컴포넌트
+// // 클래스형 컴포넌트
 
-import React, { Component } from 'react';
-import TOC from './components/TOC';
-import Content from './components/Content';
-import Subject from './components/Subject';
-import './App.css';
+// import React, { Component } from 'react';
+// import TOC from './study/TOC';
+// import Content from './study/Content';
+// import Subject from './study/Subject';
+// import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      subject: {title: 'WEB', sub:'World wide web!'},
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
-      ]
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        <Subject
-          title={this.state.subject.title}
-          sub={this.state.subject.sub}>
-        </Subject>
-        <TOC data={this.state.contents} />
-        <Content title="HTML" desc="HTML is HyperText Markup Language."/>
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       subject: {title: 'WEB', sub:'World wide web!'},
+//       contents:[
+//         {id:1, title:'HTML', desc:'HTML is for information'},
+//         {id:2, title:'CSS', desc:'CSS is for design'},
+//         {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+//       ]
+//     }
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <Subject
+//           title={this.state.subject.title}
+//           sub={this.state.subject.sub}>
+//         </Subject>
+//         <TOC data={this.state.contents} />
+//         <Content title="HTML" desc="HTML is HyperText Markup Language."/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import Movie from "./components/Movie";
+import { dummy } from "./movieDummy";
+
+function App() {
+  return (
+    <div>
+      <div className="app-container">
+        {dummy.results.map((item) => {
+          return (
+            <Movie
+              title={item.title}
+              poster_path={item.poster_path}
+              vote_average={item.vote_average}
+            />
+          );
+        })}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
