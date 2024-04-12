117b0d13b094089235151dbd4364ec78393c4177
YounHooooo <yh6316l@naver.com>
2024-04-12 23:20:15 +0900

    useRef & useState

--- src/study/TestCode.jsx
+++ src/study/TestCode.jsx
@@ -1,27 +1,50 @@
-import React, { useState, useRef } from 'react'
+// useState, useRef 
+
+import React, { useState, useRef } from 'react';
 
 export default function TestCode() {
-
     const [count, setCount] = useState(0);
-    const countRef = TestCode(0);
+    const countRef = useRef(0);
+    const [value, setValue] = useState('');
 
     console.log('렌더링...');
 
     const increaseCountState = () => {
         setCount(count + 1);
     }
+    
+    const decreaseCountState = () => {
+        setCount(count - 1);
+    }
 
     const increaseCountRef = () => {
-        increaseCountRef.current = countRef.current + 1;
+        countRef.current = countRef.current + 1;
         console.log('Ref: ', countRef.current);
     }
 
+    const decreaseCountRef = () => {
+        countRef.current = countRef.current -1;
+        console.log('Ref: ', countRef.current);
+
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    }
+
+
     return (
-        <div>
+        <>
             <p>State: {count}</p>
-            <p>Ref: {countRef.current}</p>
             <button onClick={increaseCountState}>State 증가</button>
+            <button onClick={decreaseCountState}>State 감소</button>
+
+            <p>Ref: {countRef.current}</p>
             <button onClick={increaseCountRef}>Ref 증가</button>
-        </div>
+            <button onClick={decreaseCountRef}>Ref 감소</button>
+        
+            <p>{value}</p>
+            <input type="text" value={value} onChange={handleChange}/>
+        </>
     )
 }
