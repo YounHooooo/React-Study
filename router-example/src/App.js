0b560e77df0f74febd6b3f7548881df2fc73ab8b
YounHooooo <yh6316l@naver.com>
2024-04-26 19:35:24 +0900

    Router

---
+++ router-example/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from 'react';
+import Homepage from './page/Homepage';
+import Aboutpage from './page/Aboutpage';
+import Productpage from './page/Productpage';
+import ProductDetailPage from './page/ProductDetailPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './page/LoginPage';
+import UserPage from './page/UserPage';
+
+
+function App() {
+  const [authenticate, setAuthenticate] = useState(false);
+  const PrivateRoute = () => {
+    return authenticate == true?<UserPage/>:<Navigate to="/login" />
+  };
+
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about" element={<Aboutpage />} />
+        <Route path="/products" element={<Productpage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
