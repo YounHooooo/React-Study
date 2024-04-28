a97888b4edaba8eb6f25685d8db539c38541b312
YounHooooo <yh6316l@naver.com>
2024-04-28 23:41:36 +0900

    Navbar

--- hnm-react-router-practice/src/component/Navbar.js
+++ hnm-react-router-practice/src/component/Navbar.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+
   return (
     <div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div>
+        </div>
+      </div>
+
+      <div className="nav-section">
+        <img
+          width={100}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5Ve9KTXh9baAxKg-AVXoZa0N1pFUoqYdBipZni5C6f58wKUlNhg0-ef5gjGuSZuHfdfs&usqp=CAU"
+        />
+      </div>
+
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+
+        <div>
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" />
+        </div>
+      </div>
     </div>
   );
 };
--- hnm-react-router-practice/src/page/ProductAll.js
+++ hnm-react-router-practice/src/page/ProductAll.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ProductAll = () => {
-  return <div>ProductAll</div>;
+  const getProducts = async () => {
+    let url = `http://localhost:5000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  return <div></div>;
 };
 
 export default ProductAll;
