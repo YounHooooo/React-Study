727946d58c339cb6eb651803178616d89bcc29ea
YounHooooo <yh6316l@naver.com>
2024-05-01 13:53:11 +0900

    redux

---
+++ redux-study/src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  const count = useSelector((state) => state.count);
+  const dispatch = useDispatch();
+
+  const increase = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가</button>
+    </div>
+  );
+}
+
+export default App;
