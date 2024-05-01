606dc5a507983f0f1c7d6e878d397afe9113e413
YounHooooo <yh6316l@naver.com>
2024-05-01 16:36:13 +0900

    reducer

--- redux-study/src/App.js
+++ redux-study/src/App.js
@@ -1,19 +1,34 @@
 import { useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
+import Box from "./component/Box";
 
 function App() {
-  const count = useSelector((state) => state.count);
-  const dispatch = useDispatch();
+  let count = useSelector((state) => state.count);
+  let id = useSelector((state) => state.id);
+  let password = useSelector((state) => state.password);
+  let dispatch = useDispatch();
 
   const increase = () => {
-    dispatch({ type: "INCREMENT" });
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  };
+  const login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "younho", password: "123" } });
   };
 
+  const decrease = () => {
+    dispatch({ type: "DECREMENT" });
+  };
   return (
     <div>
+      <h1>
+        {id},{password}
+      </h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={decrease}>감소</button>
+      <button onClick={login}>Login</button>
+      <Box />
     </div>
   );
 }
