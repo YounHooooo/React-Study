2e3a914a866c01bcbb10939ea96f5dcbb3cf97ba
YounHooooo <yh6316l@naver.com>
2024-05-15 15:52:16 +0900

    배포 test

--- yh/src/pages/ProductDetail.js
+++ yh/src/pages/ProductDetail.js
@@ -7,7 +7,7 @@ const ProductDetail = () => {
   let { id } = useParams();
   const [product, setProduct] = useState(null);
   const getProductDetail = async () => {
-    let url = `https://my-json-server.typicode.com/YounHooooo/yh-data/products/${id}`;
+    let url = `https://my-json-server.typicode.com/YounHooooo/React-Study/yh/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setProduct(data);
