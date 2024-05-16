d9ed104d58879ba660d8506ada76cd95de0775e0
YounHooooo <yh6316l@naver.com>
2024-05-16 18:00:00 +0900

    middleware

--- hnm-react-router-practice/src/App.js
+++ hnm-react-router-practice/src/App.js
@@ -1,30 +1,17 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
-import ProductDetail from "./page/ProductDetail";
-import Navbar from "./component/Navbar";
-import Footer from "./component/Footer";
-import PrivateRouter from "./route/PrivateRouter";
-
-// 1. 전체상품 페이지, 로그인, 상품상세페이지
-// 2. 전체상품 페이지에서는 전체 상품을 볼 수 있음
-// 3. 로그인 버튼을 누르면 로그인 페이지가 나옴
-// 3. 상품 디테일을 눌렀으나, 로그인이 안 되어있을경우에는 로그인 페이지가 먼저 나옴
-// 4. 로그인이 되어있을 경우에는 상품 디테일 페이지를 볼 수 있다
-// 5. 로그아웃 버튼을 클릭하면 로그아웃이 된다.
-// 5. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다, 다시 로그인 페이지가 보임
-// 6. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다.
-// 7. 상품을 검색할 수 있다.
+import PrivateRoute from "./Route/PrivateRoute";
 
 function App() {
-  const [authenticate, setAuthenticate] = useState(false);
-  // useEffect(() => {}, [authenticate]);
+  let [authenticate, setAuthenticate] = useState(false);
   return (
     <div>
-      <Navbar />
+      <Navbar authenticate={authenticate} setAuthenticate={setAuthenticate} />
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route
@@ -33,10 +20,9 @@ function App() {
         />
         <Route
           path="/product/:id"
-          element={<PrivateRouter authenticate={authenticate} />}
+          element={<PrivateRoute authenticate={authenticate} />}
         />
       </Routes>
-      {/* <Footer /> */}
     </div>
   );
 }
